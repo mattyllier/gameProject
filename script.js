88b2fd7812cc29cc63b842365874cb33a8f443e1
mattyllier <bloodrootfox@gmail.com>
2023-04-27 16:35:28 -0500

    gameProject

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-
+const image = document.getElementById('image')
 let level = []
 
 const tile = {
@@ -16,14 +16,16 @@ const tile = {
 const orb = {
     x : Math.random()*canvas.width-20,
     y : Math.random()*canvas.height-20,
-    radius : 20,
-    color : `#${Math.floor(Math.random()*16777215).toString(16)}`
+    radius : 7,
+    dx : 2,
+    dy: -2
+    //color : `#${Math.floor(Math.random()*16777215).toString(16)}`
 }
 
 const drawBall = (obj)=>{
     ctx.beginPath
-    ctx.fillStyle = obj.color
-    ctx.arc(obj.x,obj.y,obj.radius,0,Math.PI,false)
+    ctx.fillStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`
+    ctx.arc(obj.x,obj.y,obj.radius,0,Math.PI*2,false)
     ctx.fill()
     ctx.closePath()
     requestAnimationFrame(drawBall)
@@ -33,6 +35,7 @@ requestAnimationFrame(drawBall)
 const drawTile = (obj)=>{
     ctx.fillStyle = obj.color
     ctx.fillRect(obj.x,obj.y,obj.w,obj.h)
+    ctx.drawImage(img,10,10)
     // ctx.font = 'Tahoma 30px'
     // ctx.fillText = `${obj.text} black`
 }
@@ -42,6 +45,8 @@ const clear = ()=>{
 }
 
 window.addEventListener('keydown',e=>{
+    if(tile.x < tile.w||tile.x > canvas.width-tile.w) tile.x = tile.speed
+    if(tile.y < tile.h||tile.y > canvas.height-tile.h) tile.y = tile.speed
     if(e.key==='ArrowLeft') tile.x-=tile.speed
     if(e.key==='ArrowRight') tile.x+=tile.speed
     if(e.key==='ArrowDown') tile.y+=tile.speed
@@ -51,8 +56,8 @@ window.addEventListener('keydown',e=>{
 const draw = ()=>{
     clear()
     level.forEach(obj=>drawRect(obj))
-    drawTile(tile)
     drawBall(orb)
+    drawTile(tile)
     requestAnimationFrame(draw)
 }
 requestAnimationFrame(draw)

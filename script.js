ce214d9ea33b3e3d5a75b6ed968cd32e5a5a2c66
mattyllier <bloodrootfox@gmail.com>
2023-05-02 11:28:56 -0500

    gameProject

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 const container = document.getElementById('container')
-const header = document.getElementById('header')
+//const header = document.getElementById('header')
+const input = document.getElementById('input')
+const encry = document.getElementById('encrypt')
+const decry = document.getElementById('decrypt')
 
-
-
-const drawBall = (obj)=>{
-    ctx.beginPath
-    ctx.fillStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`
-    ctx.arc(obj.x,obj.y,obj.radius,0,Math.PI*2,false)
-    ctx.fill()
-    ctx.closePath()
-    requestAnimationFrame(drawBall)
-}
-requestAnimationFrame(drawBall)
-
-const drawTile = (obj)=>{
-    ctx.fillStyle = obj.color
-    ctx.fillRect(obj.x,obj.y,obj.w,obj.h)
-    ctx.drawImage(img,10,10)
-    // ctx.font = 'Tahoma 30px'
-    // ctx.fillText = `${obj.text} black`
-}
-
-const clear = ()=>{
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-}
-
-window.addEventListener('keydown',e=>{
-   // if(tile.x < tile.w||tile.x > canvas.width-tile.w) tile.x = tile.speed
-  //  if(tile.y < tile.h||tile.y > canvas.height-tile.h) tile.y = tile.speed
-    if(e.key==='ArrowLeft') tile.x-=tile.speed
-    if(e.key==='ArrowRight') tile.x+=tile.speed
-    if(e.key==='ArrowDown') tile.y+=tile.speed
-    if(e.key==='ArrowUp') tile.y-=tile.speed
+encry.addEventListener('click',e=>{
+    let myStr = input.value
+    const output = encrypt(myStr)
+    input.value = output
 })
 
-const draw = ()=>{
-    clear()
-    level.forEach(obj=>drawRect(obj))
-    drawBall(orb)
-    drawTile(tile)
-    requestAnimationFrame(draw)
+decry.addEventListener('click',e=>{
+    let myStr = input.value
+    const output = decrypt(myStr)
+    input.value = output
+})
+
+const encrypt = (str)=>{
+    const arr = str.toLowerCase().split('').map(a=>a.charCodeAt(0).toString(2))
+    const newArr = arr.map(a=>arr[a]=`100000000${a}`)
+    return newArr.map(a=>String.fromCharCode(parseInt(a,2))).join('')
 }
-requestAnimationFrame(draw)
+
+const decrypt = (str) =>{
+    const arr = str.split('').map(a=>a.charCodeAt(0).toString(2))
+    const newArr = arr.map(a=>a.replace(/100000000/,''))
+    return newArr.map(a=>String.fromCharCode(parseInt(a,2))).join('')
+}
+
+
+
